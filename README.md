5b2f940adc50732f04172170bbf380b39cdd1313
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2023-07-17 20:40:45 -0300

    Instalando React com VITE

--- README.md
+++
@@ -1,3 +0,0 @@
- # Exercicios Kenzie Modulo 3 - React
-
-1- Canvas atividade-1
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+ 
+//Aqui posso fazer o javaScript 
+  return (
+    //Aqui é somente o JSX 
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
