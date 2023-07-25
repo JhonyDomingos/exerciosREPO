fb82dbfe8f1c4e0146ed509de95933deed2c268f
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2023-07-25 20:41:09 -0300

    React useState aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
-
-import {PrimeiroComponente} from "./components/components"
-
-import FooterComponent from "./components/footer"
-import AboutSectionComponent from "./components/about"
-import HeaderComponent from "./components/header"
+import { useState } from "react";
 
 function App() {
+  // const [variável, funcaoParaModificarEssaVariavel] = useState(0)
+  // funcaoParaModificarEssaVariavel(novoValor)
+  // funcaoParaModificarEssaVariavel(1)
 
+  //O estado ele é semelhante a uma variável no sentido que é capaz de armazenar qualquer valor do javascript.
 
-function App() {
-  const myDatas = {
-    nome: "Jhonatan Domingos",  
-    module: "Modúlo 3",
-    age: 31, 
-  }   
-      
+  //O estado ao ter seu valor alterado , reconstrói tudo que está vinculado a ele. isso se chama reatividade
+
+  //O estado é imutavel, não pode ser alterado de forma direta
+  /*
+  const [count, setCount] = useState(0)
+
+  const addCount = () => {
+    setCount(count + 1)
+  }
+
+  const removeCount =() => {
+    setCount(count - 1)
+  }
+  */
+  const [fruitList, setFruitList] = useState([
+    {
+      id: 1,
+      name: "banana",
+      price:5.08,
+    },
+    {
+      id: 2,
+      name: "Kiwi",
+      price:8.03,
+    },
+  ]);
+
+  const total = fruitList.reduce((prevValue, fruit) => {
+    return prevValue + fruit.price
+  },0)
+
+  console.log(total)
+  const newItem = { id: 3, name: "Morango", price:13.89, };
+  const addFruit = () => {    
+     setFruitList([ ...fruitList ,newItem])
+  };
+
   return (
-
-        //Aqui é somente o JSX    
-  
-      
-      <div>
-        <li>{myDatas.nome}</li>
-        <li>{myDatas.module}</li>
-        <li>{myDatas.age}</li>
-        <PrimeiroComponente />
-      </div>
-        
-      
-   
- 
-    
-=======
-    //Aqui é somente o JSX 
-    <main className="App">
-      <HeaderComponent />
-        <AboutSectionComponent />
-        <FooterComponent />
-    </main>
-
-  )
+    <div className="App">
+      <button onClick={addFruit}>Adicionar Fruta</button>
+      {fruitList.map(fruit => (
+        <li key={fruit.id}>{fruit.name}</li>
+      ))}
+      <p>{total.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
